befdba2ca37eb7147a9f50c36c44d7851aa5e40f
brajkishor27 <40299587+brajkishor27@users.noreply.github.com>
2018-06-16 00:55:16 +0530

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,13 +2,13 @@
   <div>
     <rainbow-text text="Contact"></rainbow-text>
     <div class="links">
-      <a href="https://twitter.com/ASpittel" class="social-link" title="Twitter"><i class="fa fa-twitter blue" aria-hidden="true"></i></a>
+      <a href="https://twitter.com/braj_kishor97" class="social-link" title="Twitter"><i class="fa fa-twitter blue" aria-hidden="true"></i></a>
       <a href="https://dev.to/aspittel" class="social-link dev-to">
         <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Ali Spittel's DEV Profile" class="dev-to-img"/>
       </a>
-      <a href="https://github.com/aspittel" class="social-link" title="Github"><i class="fa fa-github purple" aria-hidden="true"></i></a>
-      <a href="https://www.linkedin.com/in/ali-spittel-768a98119" class="social-link" title="Linkedin"><i class="fa fa-linkedin dark-blue" aria-hidden="true"></i></a>
-      <a href="mailto:ali07cat07@gmail.com" class="social-link" title="email me"><i class="fa fa-envelope red" aria-hidden="true"></i></a>
+      <a href="https://github.com/brajkishor97.github.io" class="social-link" title="Github"><i class="fa fa-github purple" aria-hidden="true"></i></a>
+      <a href="https://www.linkedin.com/in/braj-kishor-230334152/" class="social-link" title="Linkedin"><i class="fa fa-linkedin dark-blue" aria-hidden="true"></i></a>
+      <a href="mailto:kumarbrajkishor96@gmail.com" class="social-link" title="email me"><i class="fa fa-envelope red" aria-hidden="true"></i></a>
       <a href="https://medium.com/on-learning-new-things" class="social-link" title="Medium"><i class="fa fa-medium green" aria-hidden="true"></i></a>
     </div>
   </div>
